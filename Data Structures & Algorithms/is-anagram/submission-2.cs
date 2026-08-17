26689b8eb134bcb5f537e7b9c4b205d0d8f50a18
Akash Mishra <mishra.akash.cloud@gmail.com>
2026-08-17 16:55:46 +0100

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        
+        if(s.Length != t.Length) return false;
+
+        int[]counts = new int[26];
+
+        for(int i = 0; i<s.Length ; i++)
+        {
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
+        }
+
+        for(int i = 0; i<26; i++)
+        {
+            if(counts[i] != 0) return false;
+        }
+
+        return true;
+    }
+}
