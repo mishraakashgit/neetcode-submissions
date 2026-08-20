57356003fdae3c37979b9731fd1defd8893cbae3
Akash Mishra <mishra.akash.cloud@gmail.com>
2026-08-20 06:25:49 +0100

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+        Dictionary<int,int> dict = new();
+        foreach (var num in nums)
+        {
+            dict[num] = dict.GetValueOrDefault(num) + 1;
+        }
+
+        PriorityQueue<int,int> heap = new();
+
+        foreach(var item in dict)
+        {
+            heap.Enqueue(item.Key, item.Value);
+            if(heap.Count > k)
+            {
+                heap.Dequeue();
+            }
+        }
+
+        int[]res = new int[k];
+
+        for(int i =0; i<k; i++)
+        {
+            heap.TryDequeue(out int task, out int priority);
+            res[i] = task;
+        }
+
+        return res;
+    }
+}
